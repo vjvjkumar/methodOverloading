25faf3663c754d8199cf2a5c487d493a077aa7fd
Vijay <vijay.t.vj@gmail.com>
2016-12-10 23:18:34 +0000

    First commit

---
+++ src/Pack1/ChildClass.java
@@ -0,0 +1,12 @@
+package Pack1;
+//Overloading: 
+public class ChildClass {
+	public static void main(String[] args) {
+		Parentclass parent=new Parentclass();
+		/*parent.sum2numbers(10, 3);
+		parent.sum3numbers(12, 23, 45);*/
+		
+		parent.sum(12, 23,34);
+	}
+
+}
---
+++ src/Pack1/Parentclass.java
@@ -0,0 +1,16 @@
+package Pack1;
+
+public class Parentclass {
+	
+	public void sum(int a,int b){
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public void sum(int i, int j, int k) {
+		int x=i+j+k;
+		System.out.println(x);
+	}
+	
+
+}
